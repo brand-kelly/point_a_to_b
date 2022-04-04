0db150b2b91e088cbb16dde287eba3f09c4b56a6
Slunchi <66961659+Slunchi@users.noreply.github.com>
2022-04-04 12:02:12 -0500

    Trying to add auto complete fragment

--- PointAtoB/app/src/main/java/com/example/pointatob/MainActivity.kt
+++ PointAtoB/app/src/main/java/com/example/pointatob/MainActivity.kt
@@ -7,5 +7,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        Places.initialize(getApplicationContext(), "AIzaSyBrYqtPVTi-L-ZvFdi0MzdrlpvPiDWvErg");
+//        PlacesClient placesClient = Places.createClient(this);
     }
 }
