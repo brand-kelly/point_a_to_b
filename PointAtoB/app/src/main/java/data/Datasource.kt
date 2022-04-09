c305add97887eaab7a9c22a8c535303beec8cccf
Slunchi <66961659+Slunchi@users.noreply.github.com>
2022-04-09 18:28:59 -0500

    Using Anthony's hard code for the user travel preferences. I commented out all of the attempts to actually sort the data in hopes there may be salvageable code :)

--- PointAtoB/app/src/main/java/data/Datasource.kt
+++ PointAtoB/app/src/main/java/data/Datasource.kt
@@ -7,8 +7,9 @@ import java.util.*
 class Datasource {
 
 
-    fun loadServices(selected : String?) : List<Service> {
+    fun loadServices(selected: String?) : List<Service> {
 
+        //Attempt 1
 //        if (selected == "Cheapest") {
 //            var myIndex = 1
 //        } else if (selected == "Fastest") {
@@ -68,16 +69,66 @@ class Datasource {
 //        )
 
 
-        val serviceList = listOf<Service>(
-            Service(R.string.serviceName1, R.integer.time1, R.integer.price1),
-            Service(R.string.serviceName2, R.integer.time2, R.integer.price2),
-            Service(R.string.serviceName3, R.integer.time3, R.integer.price3),
-            Service(R.string.serviceName4, R.integer.time4, R.integer.price4),
-            Service(R.string.serviceName5, R.integer.time5, R.integer.price5),
-            Service(R.string.serviceName6, R.integer.time6, R.integer.price6)
-        )
 
-        return serviceList
+
+//        val serviceList = mutableListOf<Service>(
+//            Service(R.string.serviceName1, R.integer.time1, R.integer.price1),
+//            Service(R.string.serviceName2, R.integer.time2, R.integer.price2),
+//            Service(R.string.serviceName3, R.integer.time3, R.integer.price3),
+//            Service(R.string.serviceName4, R.integer.time4, R.integer.price4),
+//            Service(R.string.serviceName5, R.integer.time5, R.integer.price5),
+//            Service(R.string.serviceName6, R.integer.time6, R.integer.price6)
+//        )
+
+
+        //Attempt 2
+//        var n=serviceList.size
+//        var temp:Service
+//        for(i in 0..n-1){
+//            var indexOfMin = i
+//            for(j in n-1 downTo  i){
+//                if(serviceList[j].price < serviceList[indexOfMin].price)
+//                    indexOfMin=j
+//            }
+//            if(i!=indexOfMin){
+//                temp = serviceList[i]
+//                serviceList[i]= serviceList[indexOfMin]
+//                serviceList[indexOfMin]=temp
+//            }
+//        }
+
+        //Attempt 3
+        //val sortedServiceList : MutableList<Service> = serviceList.sortedBy { Service -> Service.price } as MutableList<Service>
+
+        //hardcode
+        if (selected == "Cheapest") {
+            val serviceList = listOf<Service>(
+                Service(R.string.serviceName5, R.integer.time5, R.integer.price5),
+                Service(R.string.serviceName3, R.integer.time3, R.integer.price3),
+                Service(R.string.serviceName4, R.integer.time4, R.integer.price4),
+                Service(R.string.serviceName1, R.integer.time1, R.integer.price1),
+                Service(R.string.serviceName2, R.integer.time2, R.integer.price2),
+                Service(R.string.serviceName6, R.integer.time6, R.integer.price6))
+            return serviceList
+        } else if (selected == "Fastest") {
+            val serviceList = listOf<Service>(
+                Service(R.string.serviceName1, R.integer.time1, R.integer.price1),
+                Service(R.string.serviceName6, R.integer.time6, R.integer.price6),
+                Service(R.string.serviceName2, R.integer.time2, R.integer.price2),
+                Service(R.string.serviceName4, R.integer.time4, R.integer.price4),
+                Service(R.string.serviceName3, R.integer.time3, R.integer.price3),
+                Service(R.string.serviceName5, R.integer.time5, R.integer.price5))
+            return serviceList
+        } else {
+            val serviceList = listOf<Service>(
+                Service(R.string.serviceName1, R.integer.time1, R.integer.price1),
+                Service(R.string.serviceName2, R.integer.time2, R.integer.price2),
+                Service(R.string.serviceName3, R.integer.time3, R.integer.price3),
+                Service(R.string.serviceName4, R.integer.time4, R.integer.price4),
+                Service(R.string.serviceName5, R.integer.time5, R.integer.price5),
+                Service(R.string.serviceName6, R.integer.time6, R.integer.price6))
+            return serviceList
+        }
     }
 
 }
