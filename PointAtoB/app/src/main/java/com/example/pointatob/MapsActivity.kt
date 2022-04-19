bf1c76c469645cc465b8b11d92100c3502ec28ba
Anthony Roth-Giacinto <agiacinto@utexas.edu>
2022-04-19 13:39:38 -0500

    Map Route and Markers Finished

--- PointAtoB/app/src/main/java/com/example/pointatob/MapsActivity.kt
+++ PointAtoB/app/src/main/java/com/example/pointatob/MapsActivity.kt
@@ -1,20 +1,26 @@
 package com.example.pointatob
 
+import android.graphics.Color
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.example.pointatob.databinding.ActivityMapsBinding
+import com.google.android.libraries.places.api.Places
+import com.google.gson.Gson
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.example.pointatob.databinding.ActivityMapsBinding
-import com.google.android.gms.maps.model.LatLngBounds
-import com.google.android.gms.maps.model.Marker
-import kotlin.time.toDuration
+import com.google.android.gms.maps.model.PolylineOptions
+import okhttp3.OkHttpClient
+import okhttp3.Request
 
+@Suppress("DEPRECATION")
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
@@ -26,11 +32,35 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Check initialization of Places API
+        if (!Places.isInitialized()) {
+            Places.initialize(applicationContext, BuildConfig.MAPS_API_KEY)
+        }
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
 
+        val gd = findViewById<Button>(R.id.directions)
+            mapFragment.getMapAsync {
+                val pointA = intent.getStringExtra("pointA")
+                val pointB = intent.getStringExtra("pointB")
+                val latA = intent.extras!!.getDouble("latA")
+                val longA = intent.extras!!.getDouble("longA")
+                val starting = LatLng(latA, longA)
+                val latB = intent.extras!!.getDouble("latB")
+                val longB = intent.extras!!.getDouble("longB")
+                val destination = LatLng(latB, longB)
+
+                // Add both markers and move the camera
+                mMap = it
+                mMap.addMarker(MarkerOptions().position(starting).title("Starting from: $pointA"))
+                mMap.addMarker(MarkerOptions().position(destination).title("Destination: $pointB"))
+                val urll = getDirectionURL(starting, destination, BuildConfig.MAPS_API_KEY)
+                GetDirection(urll).execute()
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(starting, 14F))
+            }
 
 
     }
@@ -45,44 +75,94 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      * installed Google Play services and returned to the app.
      */
     override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        val sydney = LatLng(-33.852, 151.211)
-        googleMap.addMarker(
-            MarkerOptions()
-                .position(sydney)
-                .title("Marker in Sydney")
-        )
+        mMap = googleMap!!
         // Set lat and long values, place names
-        val intent = intent
-//        val latA = intent.getDoubleExtra("latA", 0.0)
-//        val latB = intent.getDoubleExtra("latB", 0.0)
-//        val longA = intent.getDoubleExtra("longA", 0.0)
-//        val longB = intent.getDoubleExtra("longB", 0.0)
-//        val starting = LatLng(latA, longA)
-//        val destination = LatLng(latB, longB)
         val pointA = intent.getStringExtra("pointA")
         val pointB = intent.getStringExtra("pointB")
-
         val latA = intent.extras!!.getDouble("latA")
         val longA = intent.extras!!.getDouble("longA")
         val starting = LatLng(latA, longA)
         val latB = intent.extras!!.getDouble("latB")
         val longB = intent.extras!!.getDouble("longB")
         val destination = LatLng(latB, longB)
-        Log.i(TAG, "latA: {$latA} is double. longA: {$longA}")
 
-
-//        LatLngBounds.Builder b = new LatLngBounds.Builder();
-//        for (Marker m : markers) {
-//
-//        }
-
-        // Add both markers and move the camera
+        mMap.clear()
         mMap.addMarker(MarkerOptions().position(starting).title(pointA))
         mMap.addMarker(MarkerOptions().position(destination).title(pointB))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(starting))
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(starting, 14F))
+    }
 
+    private fun getDirectionURL(origin:LatLng, dest:LatLng, secret: String) : String{
+        return "https://maps.googleapis.com/maps/api/directions/json?origin=${origin.latitude},${origin.longitude}" +
+                "&destination=${dest.latitude},${dest.longitude}" +
+                "&sensor=false" +
+                "&mode=driving" +
+                "&key=$secret"
+    }
+
+    private inner class GetDirection(val url : String) : AsyncTask<Void, Void, List<List<LatLng>>>(){
+        override fun doInBackground(vararg params: Void?): List<List<LatLng>> {
+            val client = OkHttpClient()
+            val request = Request.Builder().url(url).build()
+            val response = client.newCall(request).execute()
+            val data = response.body!!.string()
+
+            val result =  ArrayList<List<LatLng>>()
+            try{
+                val respObj = Gson().fromJson(data,MapData::class.java)
+                val path =  ArrayList<LatLng>()
+                for (i in 0 until respObj.routes[0].legs[0].steps.size){
+                    path.addAll(decodePolyline(respObj.routes[0].legs[0].steps[i].polyline.points))
+                }
+                result.add(path)
+            }catch (e:Exception){
+                e.printStackTrace()
+            }
+            return result
+        }
+
+        override fun onPostExecute(result: List<List<LatLng>>) {
+            val lineoption = PolylineOptions()
+            for (i in result.indices){
+                lineoption.addAll(result[i])
+                lineoption.width(10f)
+                lineoption.color(Color.GREEN)
+                lineoption.geodesic(true)
+            }
+            mMap.addPolyline(lineoption)
+        }
+    }
+
+    fun decodePolyline(encoded: String): List<LatLng> {
+        val poly = ArrayList<LatLng>()
+        var index = 0
+        val len = encoded.length
+        var lat = 0
+        var lng = 0
+        while (index < len) {
+            var b: Int
+            var shift = 0
+            var result = 0
+            do {
+                b = encoded[index++].code - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlat = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lat += dlat
+            shift = 0
+            result = 0
+            do {
+                b = encoded[index++].code - 63
+                result = result or (b and 0x1f shl shift)
+                shift += 5
+            } while (b >= 0x20)
+            val dlng = if (result and 1 != 0) (result shr 1).inv() else result shr 1
+            lng += dlng
+            val latLng = LatLng((lat.toDouble() / 1E5),(lng.toDouble() / 1E5))
+            poly.add(latLng)
+        }
+        return poly
     }
 
 }
